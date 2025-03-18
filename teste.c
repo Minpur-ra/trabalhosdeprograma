cb6e290eb8d8fcdbb1fc696bf28cac3323b03ab6
Minpur-ra <jordankarina46@gmail.com>
2025-03-18 15:13:39 -0300

    add hello world
    código teste

---
+++ teste.c
@@ -0,0 +1,17 @@
+			#include<stdio.h>
+			#include<stdlib.h>
+			int main(){
+				int numero1, numero2, numero3, media;
+				
+				printf(" digite um numero\n");
+				scanf("%d",&numero1);
+				printf(" digite um numero\n");
+				scanf("%d",&numero2);
+				printf(" digite um numero\n");
+				scanf("%d",&numero3);
+				
+				media= (numero1+numero2+numero3)/3;
+				
+				printf(" a media eh: %d", media);
+				
+			return 0;}
